64d8e994c1d5d5681f824fd962b12554f9d7198c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-11 00:34:05 +0200

    Recurse on the parent classloader (fix axel22/scalameter#22)
    When searching for URLClassLoader, try falling back (recursively) on the
    parent classloader. This finds a classloader in the setup used by SBT
    0.13.0, and it seems a sensible default heuristic in general.

--- src/main/scala/org/scalameter/package.scala
+++ src/main/scala/org/scalameter/package.scala
@@ -150,7 +150,12 @@ package object scalameter {
   def extractClasspath(classLoader: ClassLoader, default: => String): String =
     classLoader match {
       case urlclassloader: java.net.URLClassLoader => extractClasspath(urlclassloader)
-      case _ => default
+      case _ =>
+        val parent = classLoader.getParent
+        if (parent != null)
+          extractClasspath(parent, default)
+        else
+          default
     }
 
   def extractClasspath(urlclassloader: java.net.URLClassLoader): String = {
